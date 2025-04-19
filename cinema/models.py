5faaec0e892e383e9e6397246f75425a6bbe3dd2
unknown <maximuschampion2002@gmail.com>
2025-04-19 12:04:00 +0300

    Added API views for movie endpoints & Initialized Django project. - Created cinema application. - Added Movie model with fields:

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    duration = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
